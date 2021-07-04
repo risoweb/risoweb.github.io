a0efa2dfa66f2bf068853ffd00f77bb5285a51d6
risoweb <38836155+risoweb@users.noreply.github.com>
2021-07-04 16:24:30 -0300

    Update action_page.php
    Servidor conection

--- action_page.php
+++ action_page.php
@@ -1,5 +1,7 @@
+<html>
+      <body>
 <?php
-$servername = "phpmyadmin.co";
+$servername = "sql10.freemysqlhosting.net";
 $database = "sql10422696";
 $username = "sql10422696";
 $password = "c81SexNTlP";
@@ -20,3 +22,5 @@ if (mysqli_query($conn, $sql)) {
 }
 mysqli_close($conn);
 ?>
+</body>
+</html>
