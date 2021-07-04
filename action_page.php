53ff08eb2b633ebf0858db002779a32bfcbde75d
risoweb <38836155+risoweb@users.noreply.github.com>
2021-07-04 16:31:15 -0300

    Update action_page.php
    Servido MySQL

--- action_page.php
+++ action_page.php
@@ -1,26 +1,17 @@
 <html>
-      <body>
+<body>
 <?php
-$servername = "sql10.freemysqlhosting.net";
-$database = "sql10422696";
-$username = "sql10422696";
-$password = "c81SexNTlP";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
-if (!$conn) {
-      die("Connection failed: " . mysqli_connect_error());
-}
- 
-echo "Connected successfully";
- 
-$sql = "INSERT INTO geral (cliente, produto, cor, pagamento, obs, entrega) VALUES ('ok', 'ok', 'ok', '00', 'ok', '00')";
-if (mysqli_query($conn, $sql)) {
-      echo "New record created successfully";
-} else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-mysqli_close($conn);
+
+$cliente          =     $_POST["cliente"];
+$produto          =     $_POST["produto"];
+$cor              =     $_POST["cor"];
+$pagamento        =     $_POST["cliente"];
+$obs              =     $_POST["obs"];
+$entrega          =     $_POST["entrega"];
+$erro             =     0;
+      if ($erro =0){
+            include 'insere.inc'
+            }
 ?>
 </body>
 </html>
