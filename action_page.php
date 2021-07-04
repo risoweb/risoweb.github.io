9ecd6df7ae9559c1c8abab774b3353bb70200d3c
risoweb <38836155+risoweb@users.noreply.github.com>
2021-07-04 16:51:36 -0300

    Update action_page.php
    Servidor MySQL

--- action_page.php
+++ action_page.php
@@ -5,7 +5,7 @@
 $cliente          =     $_POST["cliente"];
 $produto          =     $_POST["produto"];
 $cor              =     $_POST["cor"];
-$pagamento        =     $_POST["cliente"];
+$pagamento        =     $_POST["pagamento"];
 $obs              =     $_POST["obs"];
 $entrega          =     $_POST["entrega"];
 $erro             =     0;
