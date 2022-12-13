fd1273d2e0364c9c2011c155fb66ee270b0f0210
risoweb <38836155+risoweb@users.noreply.github.com>
2022-12-13 02:56:41 +0000

    File gltf

--- react-three/src/App.js
+++ react-three/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { Canvas } from "react-three-fiber";
+import { Model } from "./Model";
+import { OrbitControls } from '@react-three/drei'
+import "./styles.scss";
+
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas>        
+        <ambientLight args={[0xff0000]} intensity={0.01} />
+        <directionalLight position={[0, 0, 5]} intensity={0.5} />
+        <directionalLight position={[0, 0, -5]} intensity={0.5} />
+        <directionalLight position={[0, 10, 0]} intensity={0.3} />
+        <Suspense fallback={null}><Model /></Suspense>
+        <OrbitControls autoRotate enableZoom={false} enablePan={false} minPolarAngle={Math.PI / 2.8} maxPolarAngle={Math.PI / 2.8} /> 
+      </Canvas>
+    </>
   );
 }
 
